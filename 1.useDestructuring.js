7c83e234b53e7426338d89ca81e632226a11eea8
aramaramyan <aramaramyan1997@mail.ru>
2021-12-14 16:45:38 +0400

    feat: create 1.useDestructuring.js file and resolve task

---
+++ 1.useDestructuring.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const person = {
+	name: "Sarah",
+	country: "Armenia",
+	job: "Developer"
+};
+
+const { name, country, job } = person;
+
+console.log(name);    // "Sarah"
+console.log(country); // "Armenia"
+console.log(job);     // "Developer"
